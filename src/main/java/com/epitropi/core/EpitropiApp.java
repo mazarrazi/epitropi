43927e8b46f1bad53f38f5f0a57ab85f6cc772b7
Mazarrazi <25sivaganesh@gmail.com>
2025-02-16 14:16:26 +0530

    Initial project setup

---
+++ src/main/java/com/epitropi/core/EpitropiApp.java
@@ -0,0 +1,11 @@
+﻿package com.epitropi.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpitropiApp {
+    public static void main(String[][] args) {
+        SpringApplication.run(EpitropiApp.class, args);
+    }
+}
